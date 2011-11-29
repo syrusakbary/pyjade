8285b92c15b17dd6198583201dbd7454a1d9cefa
Syrus Akbary <me@syrusakbary.com>
2011-11-29 21:11:57 +0100

    Bugs fixed

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup,find_packages
 
 setup(name='pyjade',
-      version = '0.5',
+      version = '0.5.1',
       download_url = 'git@github.com:syrusakbary/pyjade.git',
       packages = find_packages(),
       author = 'Syrus Akbary',
       author_email = 'me@syrusakbary.com',
       description = 'Jade syntax adapter for Django, Jinja2 and Mako templates',
+      long_description=open('README.rst').read(),
       keywords = 'jade template converter',
       url = 'http://github.com/syrusakbary/pyjade',
       license = 'MIT',
