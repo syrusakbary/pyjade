a5788e89361a183b8782cc30a964bbef6563b5b3
Syrus Akbary <me@syrusakbary.com>
2012-03-27 14:49:25 +0200

    Added Flask example

---
+++ examples/flask/run.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('hello.jade', name=name)
+
+if __name__ == "__main__":
+    app.run()
