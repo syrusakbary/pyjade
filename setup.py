70071911511645926bc0dffac5f572f3cf7bad0a
Syrus Akbary <me@syrusakbary.com>
2012-12-28 18:50:27 +0100

    Updated pyjade package version

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup,find_packages
 
 setup(name='pyjade',
-      version = '1.7',
+      version = '1.8',
       download_url = 'git@github.com:syrusakbary/pyjade.git',
       packages = find_packages(),
       author = 'Syrus Akbary',
       author_email = 'me@syrusakbary.com',
-      description = 'Jade syntax adapter for Django, Jinja2 and Mako templates',
+      description = 'Jade syntax adapter for Django, Jinja2, Mako and Tornado templates',
       long_description=open('README.rst').read(),
       keywords = 'jade template converter',
       url = 'http://github.com/syrusakbary/pyjade',
