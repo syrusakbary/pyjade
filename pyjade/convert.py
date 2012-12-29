7a8d46376fb8384a451073dba3587eda6bfb09df
Reorx <novoreorx@gmail.com>
2012-12-29 16:59:47 +0800

    Update pyjade/convert.py
    use a more convenient way to involve compilers;
    add missing tornado compiler;
    fix typo 'aviable';
    fix some informal code style
    
    Please test the code after merge it, I did do that because the change was done on web page :)

--- pyjade/convert.py
+++ pyjade/convert.py
@@ -1,61 +1,47 @@
+import logging
+import codecs
+from optparse import OptionParser
 from pyjade.utils import process
+
 def convert_file():
-    import codecs
-    aviable_compilers = {}
-    try:
-        from pyjade.ext.django import Compiler as DjancoCompiler
-        aviable_compilers['django'] = DjancoCompiler
-    except:
-        pass
-    try:
-        from pyjade.ext.jinja import Compiler as JinjaCompiler
-        aviable_compilers['jinja'] = JinjaCompiler
-    except:
-        pass
-    try:
-        from pyjade.ext.underscore import Compiler as UnderscoreCompiler
-        aviable_compilers['underscore'] = UnderscoreCompiler
-    except:
-        pass
-    try:
-        from pyjade.ext.mako import Compiler as MakoCompiler
-        aviable_compilers['mako'] = MakoCompiler
-    except:
-        pass
-    try:
-        from pyjade.ext.tornado import Compiler as TornadoCompiler
-        aviable_compilers['tornado'] = TornadoCompiler
-    except:
-        pass
-    from optparse import OptionParser
+    support_compilers_list = ['django', 'jinja', 'underscore', 'mako', 'tornado']
+    available_compilers = {}
+    for i in support_compilers_list:
+        try:
+            compiler_class = __import__('pyjade.ext.%s' % i, fromlist=['pyjade']).Compiler
+        except ImportError, e:
+            logging.warning(e)
+        else:
+            available_compilers[i] = compiler_class
+
     usage = "usage: %prog [options] file [output]"
     parser = OptionParser(usage)
     parser.add_option("-o", "--output", dest="output",
                       help="write output to FILE", metavar="FILE")
     parser.add_option("-c", "--compiler", dest="compiler",
-                        choices=['django', 'jinja', 'mako', 'underscore',],
-                        default='django',
-                        type="choice",
-                      help="COMPILER must be django (default), jinja, underscore or mako ")
+                      choices=available_compilers.keys(),
+                      default='django',
+                      type="choice",
+                      help="COMPILER must be one of %s, default is django" % ','.join(available_compilers.keys()))
     # parser.add_option("-q", "--quiet",
     #                   action="store_false", dest="verbose", default=True,
     #                   help="don't print status messages to stdout")
-    (options, args) = parser.parse_args()
-    if len(args)<1:
+    options, args = parser.parse_args()
+    if len(args) < 1:
         print "Specify the input file as the first argument."
         exit()
-    file_output = options.output or (args[1] if len(args)>1 else None)
+    file_output = options.output or (args[1] if len(args) > 1 else None)
     compiler = options.compiler
-    if compiler in aviable_compilers:
+    if compiler in available_compilers:
         template = codecs.open(args[0], 'r', encoding='utf-8').read()
-        output = process(template,compiler=aviable_compilers[compiler],staticAttrs=True)
+        output = process(template, compiler=available_compilers[compiler], staticAttrs=True)
         if file_output:
             outfile = codecs.open(file_output, 'w', encoding='utf-8')
             outfile.write(output)
         else:
             print output
     else:
-        raise Exception('You must have %s installed!'%compiler)
+        raise Exception('You must have %s installed!' % compiler)
     # print options,args
 
 if __name__ == '__main__':
