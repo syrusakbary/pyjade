e97fd2e8b9183b7d822722fbda6c3c2c1b154660
Syrus Akbary <me@syrusakbary.com>
2013-01-07 00:28:57 +0100

    Updated package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(name='pyjade',
-      version = '1.8',
+      version = '1.9',
       download_url = 'git@github.com:syrusakbary/pyjade.git',
       packages = find_packages(),
       author = 'Syrus Akbary',
