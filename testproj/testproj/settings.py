9ec8c77e6b04866632c8ea5f7327e23e712575a4
Andrew Boltachev <andrewboltachev@gmail.com>
2013-08-08 01:56:13 +0400

    Modified testproj settings

--- testproj/testproj/settings.py
+++ testproj/testproj/settings.py
@@ -1,6 +1,8 @@
 import sys
+import os
 
-sys.path.append('/home/andrey/my/pyjade')
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
+
 # Django settings for testproj project.
 
 DEBUG = True
