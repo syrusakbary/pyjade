7491495dcb53e526184c4c90fdc82899f0e79048
john <john@bataille>
2015-05-23 09:46:17 +0200

    Add convenience function to process jade template inside python script directly

--- pyjade/__init__.py
+++ pyjade/__init__.py
@@ -3,3 +3,6 @@ from .parser import Parser
 from .compiler import Compiler
 from .utils import process
 from .filters import register_filter
+from .ext import html
+
+simple_convert = lambda t: html.process_jade(t)
